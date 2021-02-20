f56e3a266837dced4900984ec8e206d987c9c557
Alexander Lozovsky <lozovsky.alx@yandex.ru>
2021-02-21 01:31:47 +0300

    fix lint errors

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,13 +1,14 @@
 #! usr/bin/env python3
 
 """brain_games."""
-from brain_games.cli import welcome_user, ask_user_name
+from brain_games.cli import ask_user_name, welcome_user
 
 
 def main():
     """Run greetings."""
     welcome_user()
     ask_user_name()
+
 
 if __name__ == '__main__':
     main()
