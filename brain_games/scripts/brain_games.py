abe0402c287729dcca241c9372966efe4fe7a471
Alexander Lozovsky <lozovsky.alx@yandex.ru>
2021-02-21 01:29:48 +0300

    fix brain-games

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,13 +1,13 @@
 #! usr/bin/env python3
 
 """brain_games."""
-from brain_games.cli import welcome_user
+from brain_games.cli import welcome_user, ask_user_name
 
 
 def main():
     """Run greetings."""
     welcome_user()
-
+    ask_user_name()
 
 if __name__ == '__main__':
     main()
